6efdd7cbfc9644c99fa9a3634e5829d29419c14c
Tanouu <ethanpachjeco.t@gmail.com>
2024-11-03 14:33:22 +0100

    passage java 17

--- backend.projet2/src/main/java/com/tanou/projet/oc/backend/projet2/controller/UserController.java
+++ backend.projet2/src/main/java/com/tanou/projet/oc/backend/projet2/controller/UserController.java
@@ -15,22 +15,25 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
+import java.util.Map;
+
 @RestController
 @RequestMapping("/api/auth")
 public class UserController {
 
-    private final UserService userService;
-    private final PasswordEncoder passwordEncoder;
-    private final UserDetailsServiceImpl userDetailsService;
-    private final JwtGenerator jwtGenerator;
+  private final UserService userService;
+  private final PasswordEncoder passwordEncoder;
+  private final UserDetailsServiceImpl userDetailsService;
+  private final JwtGenerator jwtGenerator;
 
-    @Autowired
-    public UserController(UserService userService, PasswordEncoder passwordEncoder, UserDetailsServiceImpl userDetailsService, JwtGenerator jwtUtil) {
-        this.userService = userService;
-        this.passwordEncoder = passwordEncoder;
-        this.userDetailsService = userDetailsService;
-        this.jwtGenerator = jwtUtil;
-    }
+  @Autowired
+  public UserController(UserService userService, PasswordEncoder passwordEncoder, UserDetailsServiceImpl userDetailsService, JwtGenerator jwtUtil) {
+    this.userService = userService;
+    this.passwordEncoder = passwordEncoder;
+    this.userDetailsService = userDetailsService;
+    this.jwtGenerator = jwtUtil;
+  }
 
   @PostMapping("/register")
   public ResponseEntity<?> registerNewUser(@RequestBody UserDto userDto) {
@@ -41,18 +44,19 @@ public class UserController {
   }
 
   @PostMapping("/email")
-  public ResponseEntity<String> loginUser(@RequestBody LoginDto loginData) {
+  public ResponseEntity<Map<String, String>> loginUser(@RequestBody LoginDto loginData) {
     User user = userService.findUserByEmail(loginData.getLogin());
     if (user == null) {
-      return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("User not found");
+      return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(Map.of("error", "User not found"));
     } else if (!passwordEncoder.matches(loginData.getPassword(), user.getPassword())) {
-      return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Invalid username/password");
+      return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(Map.of("error", "Invalid username/password"));
     }
-    final UserDetails userDetails = userDetailsService.loadUserByUsername(user.getEmail());
     UsernamePasswordAuthenticationToken authentication =
-      new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
+      new UsernamePasswordAuthenticationToken(user.getEmail(), null, new ArrayList<>());
+
     final String jwt = jwtGenerator.generateToken(authentication);
 
-    return ResponseEntity.ok(jwt);
+    return ResponseEntity.ok(Map.of("token", jwt));
   }
 }
+
